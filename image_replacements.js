51fc46d55dbaa50314a17ea6a6e1c2eb07e2e705
Testanki1 <104706055+Testanki1@users.noreply.github.com>
2025-08-02 15:29:44 +0800

    Update image_replacements.js

--- image_replacements.js
+++ image_replacements.js
@@ -1,4 +1,4 @@
 const imageReplacementDict = {
-    "https://en.tankiwiki.com/images/en/c/c8/Garage_background_2023.jpg": "https://your-cdn.com/garage.jpg"
+    "https://en.tankiwiki.com/images/en/e/eb/Hulls.png": "https://en.tankiwiki.com/images/en/b/b9/Turrets.png"
     
 };
