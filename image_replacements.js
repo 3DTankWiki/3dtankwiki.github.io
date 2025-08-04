ac3348529795c503fad0d6e7d9650b3735f50c05
Testanki1 <104706055+Testanki1@users.noreply.github.com>
2025-08-04 22:17:50 +0800

    Update image_replacements.js

--- image_replacements.js
+++ image_replacements.js
@@ -1,3 +1,5 @@
 const imageReplacementDict = {
-    "https://en.tankiwiki.com/images/en/thumb/0/0c/Ranks_en2.jpg/1542px-Ranks_en2.jpg":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg"
+    "https://en.tankiwiki.com/images/en/thumb/0/0c/Ranks_en2.jpg/1542px-Ranks_en2.jpg":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg",
+    "https://en.tankiwiki.com/images/en/thumb/0/0c/Ranks_en2.jpg/1157px-Ranks_en2.jpg":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg",
+    "https://en.tankiwiki.com/images/en/thumb/0/0c/Ranks_en2.jpg/771px-Ranks_en2.jpg":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg"
 };
