54e4153e913558237776c91257aad548f76a0caa
Testanki1 <104706055+Testanki1@users.noreply.github.com>
2025-08-06 18:15:08 +0800

    Update image_replacements.js

--- image_replacements.js
+++ image_replacements.js
@@ -2,5 +2,6 @@ const imageReplacementDict = {
     "https://en.tankiwiki.com/images/en/thumb/0/0c/Ranks_en2.jpg/1542px-Ranks_en2.jpg":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg",
     "https://en.tankiwiki.com/images/en/thumb/0/0c/Ranks_en2.jpg/1157px-Ranks_en2.jpg":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg",
     "https://en.tankiwiki.com/images/en/thumb/0/0c/Ranks_en2.jpg/771px-Ranks_en2.jpg":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg",
-    "https://en.tankiwiki.com/images/en/0/07/RanksImageNew.png":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg"
+    "https://en.tankiwiki.com/images/en/0/07/RanksImageNew.png":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg",
+    "https://en.tankiwiki.com/images/en/thumb/2/26/Gamepad_controls.jpg/1800px-Gamepad_controls.jpg":"https://i.ibb.co/RTPdqgrs/1754475033490.jpg"
 };
