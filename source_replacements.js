adc65de5af73c4fd1f3d384be9e22d0b4ec68f67
Testanki1 <104706055+Testanki1@users.noreply.github.com>
2025-10-01 23:12:54 +0800

    Update source_replacements.js

--- source_replacements.js
+++ source_replacements.js
@@ -1,5 +1,5 @@
 const sourceReplacementDict = {
-    //视频链接
+    //绝对链接
     "https://en.tankiwiki.com/images/en/thumb/0/0c/Ranks_en2.jpg/1542px-Ranks_en2.jpg":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg",
     "https://en.tankiwiki.com/images/en/thumb/0/0c/Ranks_en2.jpg/1157px-Ranks_en2.jpg":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg",
     "https://en.tankiwiki.com/images/en/thumb/0/0c/Ranks_en2.jpg/771px-Ranks_en2.jpg":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg",
