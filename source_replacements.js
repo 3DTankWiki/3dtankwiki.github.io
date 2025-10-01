98c0fd5c00241bf212eb3a6febf38faeee8c3140
Testanki1 <104706055+Testanki1@users.noreply.github.com>
2025-10-01 23:12:25 +0800

    Update source_replacements.js

--- source_replacements.js
+++ source_replacements.js
@@ -1,4 +1,5 @@
 const sourceReplacementDict = {
+    //视频链接
     "https://en.tankiwiki.com/images/en/thumb/0/0c/Ranks_en2.jpg/1542px-Ranks_en2.jpg":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg",
     "https://en.tankiwiki.com/images/en/thumb/0/0c/Ranks_en2.jpg/1157px-Ranks_en2.jpg":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg",
     "https://en.tankiwiki.com/images/en/thumb/0/0c/Ranks_en2.jpg/771px-Ranks_en2.jpg":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg",
@@ -6,6 +7,7 @@ const sourceReplacementDict = {
     "https://en.tankiwiki.com/images/en/thumb/2/26/Gamepad_controls.jpg/900px-Gamepad_controls.jpg":"https://i.ibb.co/RTPdqgrs/1754475033490.jpg",
     "https://en.tankiwiki.com/images/en/thumb/2/26/Gamepad_controls.jpg/1350px-Gamepad_controls.jpg":"https://i.ibb.co/RTPdqgrs/1754475033490.jpg",
     "https://en.tankiwiki.com/images/en/thumb/2/26/Gamepad_controls.jpg/1800px-Gamepad_controls.jpg":"https://i.ibb.co/RTPdqgrs/1754475033490.jpg",
-    "https://www.youtube-nocookie.com/embed/Wm0gOM_vchI?":"https://player.bilibili.com/player.html?bvid=BV1RDn5zTEvA&p=1",
-    "trXeSeVUeAI":"BV1RDn5zTEvA"
+    "https://www.youtube-nocookie.com/embed/Wm0gOM_vchI?":"https://player.bilibili.com/player.html?bvid=BV1RDn5zTEvA&p=1"
+    
+    //视频 ID（如："trXeSeVUeAI":"BV1RDn5zTEvA"）
 };
