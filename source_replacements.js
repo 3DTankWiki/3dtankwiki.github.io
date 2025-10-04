7ad4655d607ac6d7dc6ce9b233b18cbf80c4215f
Testanki1 <104706055+Testanki1@users.noreply.github.com>
2025-10-04 17:13:36 +0800

    Update source_replacements.js

--- source_replacements.js
+++ source_replacements.js
@@ -1,8 +1,8 @@
 const sourceReplacementDict = {
     //绝对链接：
-    "https://en.tankiwiki.com/images/en/thumb/0/0c/Ranks_en2.jpg":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg",
+    "https://en.tankiwiki.com/images/en/0/0c/Ranks_en2.jpg":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg",
     "https://en.tankiwiki.com/images/en/0/07/RanksImageNew.png":"https://i.ibb.co/5ggf1jhJ/1752753785939.jpg",
-    "https://en.tankiwiki.com/images/en/thumb/2/26/Gamepad_controls.jpg":"https://i.ibb.co/RTPdqgrs/1754475033490.jpg",
+    "https://en.tankiwiki.com/images/en/2/26/Gamepad_controls.jpg":"https://i.ibb.co/RTPdqgrs/1754475033490.jpg",
     "https://www.youtube-nocookie.com/embed/Wm0gOM_vchI?":"https://player.bilibili.com/player.html?bvid=BV1RDn5zTEvA",
     "https://www.youtube-nocookie.com/embed/IHLaSiVgEtQ?":"https://player.bilibili.com/player.html?bvid=BV1oPn5zBEZ5",
     "https://www.youtube-nocookie.com/embed/3LoNmAm3BGs?":"https://player.bilibili.com/player.html?bvid=BV1ZAn5zuEet",
@@ -11,14 +11,14 @@ const sourceReplacementDict = {
     "https://www.youtube-nocookie.com/embed/vKeeAq8xIhA?":"https://player.bilibili.com/player.html?bvid=BV1dKn5zTEZw",
     "https://en.tankiwiki.com/images/en/8/86/Lobbywindows.png":"https://i.ibb.co/6kjWndX/2025-10-04-100537.png",
     "https://en.tankiwiki.com/images/en/2/2a/Challenge_Window.png":"https://i.ibb.co/0yKG07kS/2025-10-04-152000.png",
-    "https://en.tankiwiki.com/images/en/thumb/6/68/MmPlayButton.png":"https://i.ibb.co/LXmd5YjK/2025-10-04-152505.png",
+    "https://en.tankiwiki.com/images/en/6/68/MmPlayButton.png":"https://i.ibb.co/LXmd5YjK/2025-10-04-152505.png",
     "https://en.tankiwiki.com/images/en/9/9d/Bmodes.png":"https://i.ibb.co/PZFX6XX5/2025-10-04-152925.png",
     "https://en.tankiwiki.com/images/en/8/88/Garage.png":"https://i.ibb.co/v6ZCMmrX/2025-10-04-155948.png",
     "https://en.tankiwiki.com/images/en/a/ad/Lobby_play_button.png":"https://i.ibb.co/wZ5HfDZ2/2025-10-04-155414.png",
     "https://en.tankiwiki.com/images/en/7/7a/Create_A_Battle_List.png":"https://i.ibb.co/VWy5L0FJ/2025-10-04-155725.png",
-    "https://en.tankiwiki.com/images/en/thumb/7/70/Interface_main_view.png":"https://i.ibb.co/5WXqCtkt/2025-10-04-160805.png",
-    "https://en.tankiwiki.com/images/en/thumb/2/25/Battle_Interface2.png":"https://i.ibb.co/sJ1H8G56/2025-10-04-160819.png",
-    "https://en.tankiwiki.com/images/en/thumb/8/80/PauseMenu1.png":"https://i.ibb.co/5yXkjwx/2025-10-04-160842.png",
+    "https://en.tankiwiki.com/images/en/7/70/Interface_main_view.png":"https://i.ibb.co/5WXqCtkt/2025-10-04-160805.png",
+    "https://en.tankiwiki.com/images/en/2/25/Battle_Interface2.png":"https://i.ibb.co/sJ1H8G56/2025-10-04-160819.png",
+    "https://en.tankiwiki.com/images/en/8/80/PauseMenu1.png":"https://i.ibb.co/5yXkjwx/2025-10-04-160842.png",
     "https://en.tankiwiki.com/images/en/0/04/Battle_stats_window.png":"https://i.ibb.co/PvDHjJ2L/2025-10-04-162040.png",
     "https://en.tankiwiki.com/images/en/a/a7/Garage_screen.png":"https://i.ibb.co/LdmQP1wR/2025-10-04-164633.png"
     //弹窗视频 ID：（例如："trXeSeVUeAI":"BV1RDn5zTEvA"）
