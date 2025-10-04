d77a83669e8b651c06db6f31f30362fa6e09c962
Testanki1 <104706055+Testanki1@users.noreply.github.com>
2025-10-04 10:26:23 +0800

    Update source_replacements.js

--- source_replacements.js
+++ source_replacements.js
@@ -9,11 +9,11 @@ const sourceReplacementDict = {
     "https://en.tankiwiki.com/images/en/thumb/2/26/Gamepad_controls.jpg/1800px-Gamepad_controls.jpg":"https://i.ibb.co/RTPdqgrs/1754475033490.jpg",
     "https://www.youtube-nocookie.com/embed/Wm0gOM_vchI?":"https://player.bilibili.com/player.html?bvid=BV1RDn5zTEvA",
     "https://www.youtube-nocookie.com/embed/IHLaSiVgEtQ?":"https://player.bilibili.com/player.html?bvid=BV1oPn5zBEZ5",
-    "https://www.youtube-nocookie.com/embed/3LoNmAm3BGs?","https://player.bilibili.com/player.html?bvid=BV1ZAn5zuEet",
-    "https://www.youtube-nocookie.com/embed/VykRhEe9LpI?","https://player.bilibili.com/player.html?bvid=BV1jTn5zqEE8",
-    "https://www.youtube-nocookie.com/embed/B0aGEXzwwvk?","https://player.bilibili.com/player.html?bvid=BV1oKn5zTExU",
-    "https://www.youtube-nocookie.com/embed/vKeeAq8xIhA?","https://player.bilibili.com/player.html?bvid=BV1dKn5zTEZw",
-    "https://en.tankiwiki.com/images/en/8/86/Lobbywindows.png","https://i.ibb.co/6kjWndX/2025-10-04-100537.png",
-    "https://en.tankiwiki.com/images/en/thumb/8/86/Lobbywindows.png/1200px-Lobbywindows.png","https://i.ibb.co/6kjWndX/2025-10-04-100537.png"
+    "https://www.youtube-nocookie.com/embed/3LoNmAm3BGs?":"https://player.bilibili.com/player.html?bvid=BV1ZAn5zuEet",
+    "https://www.youtube-nocookie.com/embed/VykRhEe9LpI?":"https://player.bilibili.com/player.html?bvid=BV1jTn5zqEE8",
+    "https://www.youtube-nocookie.com/embed/B0aGEXzwwvk?":"https://player.bilibili.com/player.html?bvid=BV1oKn5zTExU",
+    "https://www.youtube-nocookie.com/embed/vKeeAq8xIhA?":"https://player.bilibili.com/player.html?bvid=BV1dKn5zTEZw",
+    "https://en.tankiwiki.com/images/en/8/86/Lobbywindows.png":"https://i.ibb.co/6kjWndX/2025-10-04-100537.png",
+    "https://en.tankiwiki.com/images/en/thumb/8/86/Lobbywindows.png/1200px-Lobbywindows.png":"https://i.ibb.co/6kjWndX/2025-10-04-100537.png"
     //弹窗视频 ID（例如："trXeSeVUeAI":"BV1RDn5zTEvA"）
 };
